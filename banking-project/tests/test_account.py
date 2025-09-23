56b39a195a7cdfbe9e74e6c62e7e0bbe536fd1fe
Malak55448 <malakalanazi0@gmail.com>
2025-09-23 06:53:31 +0300

    Add unit tests for Account deposit, withdraw, and transfer

--- banking-project/tests/test_account.py
+++ banking-project/tests/test_account.py
@@ -22,4 +22,20 @@ class MyAccountTests(unittest.TestCase):
         self.testAccount.deposit("savings", 40)
         self.assertEqual(self.testAccount.balance_savings, 50)
 
+    
+    def test_withdrawSavings(self):
+        self.testAccount.withdraw("savings", 5)
+        self.assertEqual(self.testAccount.balance_savings, 5)
 
+    def test_transferCheckToSavings(self):
+        self.testAccount.transfer("checking", "savings", 5) #transfer form checking --> savings
+        self.assertEqual(self.testAccount.balance_checking, 5)
+        self.assertEqual(self.testAccount.balance_savings, 15)
+
+    def test_transferSavingsToCheck(self):
+        self.testAccount.transfer("savings", "checking", 5)#transfer form savings  --> checking
+        self.assertEqual(self.testAccount.balance_savings, 5)
+        self.assertEqual(self.testAccount.balance_checking, 15)
+
+if __name__ == "__main__":
+    unittest.main()
