f9550f36e4b8f1658ee0c3d1c1cd5d110505f3fb
Malak55448 <malakalanazi0@gmail.com>
2025-09-24 20:23:29 +0300

    Add deposit methods and tests (checking, savings, invalid cases)

--- banking-project/tests/test_account.py
+++ banking-project/tests/test_account.py
@@ -20,6 +20,42 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(account.checking_balance, 1000.0)
         self.assertEqual(account.savings_balance, 5000.0)
         print("The account was successfully created")
+
+    def test_three_DepositCheck(self):
+        from bank.account import Account
+        account = Account("10001", 1000.0, 5000.0)
+
+        BalanceNew = account.deposit_checking(500.0)
+
+        self.assertEqual(BalanceNew, 1500.0)
+        self.assertEqual(account.checking_balance, 1500.0)
+        print("Deposited into the current account")
+
+    def test_four_DepositSav(self):
+        from bank.account import Account
+        account = Account("10001", 1000.0, 5000.0)
+
+        BalanceNew = account.deposit_savings(1000.0)
+        
+        self.assertEqual(BalanceNew, 6000.0)
+        self.assertEqual(account.savings_balance, 6000.0)
+        print("funds have been deposited into the savings account")
+
+
+    def test_five_invDeposit(self):
+        from bank.account import Account
+        account = Account("10001", 1000.0, 5000.0)
+      
+
+        with self.assertRaises(ValueError): #depositing a negative amoount is wrong -100 ≤ 0
+            account.deposit_checking(-100.0)
+
+
+        with self.assertRaises(ValueError):#: 0 ≤ 0
+            account.deposit_checking(0)
+        print("preventing in correct deposits") 
+
+
 if __name__ =='__main__':
     print("start account tests")
     unittest.main(verbosity= 2)
