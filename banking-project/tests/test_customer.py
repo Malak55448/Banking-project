d7f6bb6ef12735bdfe606904c5872325bce3caea
Malak55448 <malakalanazi0@gmail.com>
2025-09-25 22:10:19 +0300

    test new customer creation

---
+++ banking-project/tests/test_customer.py
@@ -0,0 +1,37 @@
+
+import unittest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+class TestMyCustomer(unittest.TestCase):
+
+    def test_one_CustomerCreat(self):
+        from bank.customer import Customer
+        customer = Customer("10001", "Malak", "Ali", "malak123", 1000.0, 5000.0)
+
+        self.assertEqual(customer.account_id, "10001")
+        self.assertEqual(customer.first_name, "Malak")
+        self.assertEqual(customer.last_name, "Ali") 
+        self.assertEqual(customer.password, "malak123")
+        self.assertEqual(customer.checking_balance, 1000.0)
+        self.assertEqual(customer.savings_balance, 5000.0)
+        self.assertEqual(customer.overdraft_count, 0)
+        self.assertTrue(customer.is_active)
+        print(" Customer created successfully!")
+
+
+
+
+
+
+
+
+
+
+
+        
+if __name__ == '__main__':
+    print("customer tests")
+    unittest.main(verbosity=2)
